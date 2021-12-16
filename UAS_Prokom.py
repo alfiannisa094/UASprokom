cd71fe575a89e30ca778bdf75c131872034f21c9
alfiannisa094 <96229000+alfiannisa094@users.noreply.github.com>
2021-12-16 22:12:26 +0800

    Update UAS_Prokom.py

--- UAS_Prokom.py
+++ UAS_Prokom.py
@@ -98,11 +98,21 @@ slider_year = st.slider("Select year", min_value = 1971, max_value = 2015)
 df2_year = df_cleaned.loc[df_cleaned["tahun"] == int(slider_year),["kode_negara","produksi"]]
 df2_sorted = df2_year.sort_values(["produksi"], ascending = False)  #mengurutkan data produksi dari terbesar ke terkecil
 df2_reindexed = df2_sorted.reset_index(drop=True)
-df2_final = df2_reindexed[0:int(input_country)]
+df2 = df2_reindexed[0:int(input_country)]
+
+df2_list = df2["kode_negara"].tolist()
+
+df2_countries = list()
+for i in df2_list:
+    df2_countries.append(countryncode[i])
+
+n = df2.columns[0]
+df2.drop(n, axis = 1, inplace = True)
+df2[n] = df2_countries
 
 st.subheader(f"Top {int(input_country)} Crude Oil Producers in {slider_year}")
 fig2, ax = plt.subplots()
-ax.bar(df2_final["kode_negara"], df2_final["produksi"])
+ax.bar(df2_final["kode_negara"], df2["produksi"])
 ax.set(xlabel = "Country", ylabel = "Crude Oil Production")
 plt.setp(ax.get_xticklabels(), rotation = 90)
 st.pyplot(fig2)
