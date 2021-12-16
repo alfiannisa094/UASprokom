375e9c25a8f4cdd76ecbd7c0cc4674cd8dde285f
alfiannisa094 <96229000+alfiannisa094@users.noreply.github.com>
2021-12-16 18:12:01 +0800

    Update UAS_Prokom.py

--- UAS_Prokom.py
+++ UAS_Prokom.py
@@ -98,7 +98,7 @@ slider_year = st.slider("Select year", min_value = 1971, max_value = 2015)
 df2_year = df_cleaned.loc[df_cleaned["tahun"] == int(slider_year),["kode_negara","produksi"]]
 df2_sorted = df2_year.sort_values(["produksi"], ascending = False)  #mengurutkan data produksi dari terbesar ke terkecil
 df2_reindexed = df2_sorted.reset_index(drop=True)
-df2_final = df2_reindexed[0:int(slider1_country)]
+df2_final = df2_reindexed[0:int(slider_country)]
 
 fig2, ax = plt.subplots()
 ax.bar(df2_final["kode_negara"], df2_final["produksi"])
